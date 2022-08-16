1f62464fbabc4fafc8d7ed2ddbcfc8094d71107d
DoZerrro <110924797+DoZerrro@users.noreply.github.com>
2022-08-16 19:37:10 +0900

    develop 테스트 전 commit

--- src/com/douzonetenten/tentenboardclient/view/AlertView.java
+++ src/com/douzonetenten/tentenboardclient/view/AlertView.java
@@ -1,12 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.douzonetenten.tentenboardclient.view;
 
 import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
 
 public class AlertView {
     private static LocalDateTime getTime = LocalDateTime.now();
 
-    public static void displayError(String message){
+    public AlertView() {
+    }
+
+    public static void displayError(String message) {
         System.out.println("[ERROR]" + getTime + " : " + message);
     }
 }
