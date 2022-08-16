c9c17fdc8dc88498ad61eedc38056c1516cbc6ef
DESKTOP-0P3C0Q6\snqpil <feel8079@gmail.com>
2022-08-16 23:53:19 +0900

    직원용 게시판//게시글 작성 기능 추가
    매개변수 개념을 잘 몰라서 구현하지 못했음

--- src/com/douzonetenten/tentenboardclient/view/EmpBoardView.java
+++ src/com/douzonetenten/tentenboardclient/view/EmpBoardView.java
@@ -1,7 +1,10 @@
 package com.douzonetenten.tentenboardclient.view;
 
 import com.douzonetenten.tentenboardclient.controller.PostController;
+import com.douzonetenten.tentenboardclient.dto.BoardDto;
 import com.douzonetenten.tentenboardclient.dto.JoinPostDto;
+import com.douzonetenten.tentenboardclient.dto.PostDto;
+import com.douzonetenten.tentenboardclient.dto.UserDto;
 import com.douzonetenten.tentenboardclient.view.feature.Display;
 
 import java.util.ArrayList;
@@ -10,6 +13,12 @@ import java.util.Scanner;
 public class EmpBoardView {
 
     PostController postController = new PostController();
+
+    PostDto postDto = new PostDto();
+
+    UserDto userDto = new UserDto();
+
+    BoardDto boardDto = new BoardDto();
 
     public void start(String selectNum) {
         Scanner scanner = new Scanner(System.in);
@@ -34,6 +43,9 @@ public class EmpBoardView {
         System.out.print("조회할 게시글 번호를 입력하세요 : ");
         String selectPost = scanner.next();
         // b. 뒤로가기, n. 다음 페이지, f. 이전 페이지, w. 글쓰기로 이동
+        if (selectPost == "w") { /* 게시글 쓰기 (CREATE) */
+            insertPost(PostDto postDto); // ??
+        }
         // 6번 게시글 조회를 어떻게 할것인가? 입력받은 숫자 == post_id
         /* 게시글 상세조회 (READ) */
         for (JoinPostDto joinPostDto : getPostList) {
@@ -53,7 +65,7 @@ public class EmpBoardView {
         /* 게시글 삭제 (DELETE) */
         if ( move == "d" ) {
             System.out.println("정말 게시물을 삭제하시겠습니까? (Y/N)");
-            char ans = scanner.next().charAt();
+            char ans = scanner.next().charAt(0);
             if ( ans == 'y' || ans == 'Y') {
                 postController.deletePost(selectPost); // selectPost 변수 재활용
             } else if ( ans == 'n' || ans == 'N' ) {
@@ -61,6 +73,26 @@ public class EmpBoardView {
             } else {
                 System.out.println("유효하지 않은 입력입니다. 다시 입력해주세요."); // 반복문으로 수정해야 질문으로 돌아갈 듯
             }
+        }
+
+        // 메소드 선언 및 호출에서 모르겠음
+        public static void insertPost(PostDto postDto) { /* 글쓰기 메소드 */
+            Display.clearConsole();
+            Display.displayTitle("글쓰기");
+            postDto.setUserNo(userDto.getUserNo()); // 로그인한 회원의 넘버 가져옴
+            postDto.setBoardNo(boardDto.getBoardNo()); // 직원용 게시판 넘버 강제 셋팅
+            System.out.println("글 제목을 입력하세요.");
+            postDto.setPostTitle(scanner.nextLine());
+            System.out.println("글 내용을 입력하세요.");
+            postDto.setPostBody(scanner.nextLine());
+
+            int result = postController.insertPost(postDto, ); // 매개변수....뭐지
+
+            if (result == 1) {
+                System.out.println("게시글이 작성되었습니다.");
+            }
+
+
         }
 
 
