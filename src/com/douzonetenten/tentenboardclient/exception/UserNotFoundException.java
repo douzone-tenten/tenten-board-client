0e73d8a16e255afd14f0c8607631488b17e4a23d
d0uwhs <d0uwhs@gmail.com>
2022-08-16 10:58:38 +0900

    Fix: JDBC Driver Properties

--- src/com/douzonetenten/tentenboardclient/exception/UserNotFoundException.java
+++ src/com/douzonetenten/tentenboardclient/exception/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package com.douzonetenten.tentenboardclient.exception;public class UserNotFoundException {
+package com.douzonetenten.tentenboardclient.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(){
+        super("해당 유저를 찾을 수 없습니다.");
+    }
 }
