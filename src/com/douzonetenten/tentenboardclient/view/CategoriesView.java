22745dcb9d3880ac4894f339d11a89f0c705e89f
suyeon <tndus3822@gmail.com>
2022-08-18 17:31:22 +0900

    classOne 기능 구현 완료

--- src/com/douzonetenten/tentenboardclient/view/CategoriesView.java
+++ src/com/douzonetenten/tentenboardclient/view/CategoriesView.java
@@ -7,26 +7,28 @@ import com.douzonetenten.tentenboardclient.view.boards.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static com.douzonetenten.tentenboardclient.utils.ConsoleUtils.clearConsole;
-import static com.douzonetenten.tentenboardclient.utils.ConsoleUtils.logInfo;
+import static com.douzonetenten.tentenboardclient.service.UserService.loginUserContext;
+import static com.douzonetenten.tentenboardclient.utils.ConsoleUtils.*;
 import static com.douzonetenten.tentenboardclient.utils.UserInterfaceUtils.uiTitle;
 
 public class CategoriesView {
     FreeBoardView boardView = new FreeBoardView();
-    ClassOneVIew classOneVIew = new ClassOneVIew();
+    ClassOneView classOneView = new ClassOneView();
     ClassTwoView douZoneTwoView = new ClassTwoView();
     AnonymousView anonymousView = new AnonymousView();
     BoardController boardController = new BoardController();
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
-        clearConsole();
-        uiTitle("전체 게시판 목록");
+
         ArrayList<BoardDto> boardDtoArrayList = boardController.findAllByBoard();
+        while (true) {
+            clearConsole();
+            uiTitle("전체 게시판 목록");
         for (BoardDto boardDto : boardDtoArrayList) {
             System.out.println(boardDto.toStringByAll());
         }
-        while (true) {
+
             System.out.print("\n어느 게시판을 선택하시겠습니까? : (뒤로가기 : 9)");
             String selectNum = scanner.next();
             if (selectNum.equals("1")) {
@@ -40,7 +42,19 @@ public class CategoriesView {
                 nmv.start("3");
             }
             if (selectNum.equals("4")) { //1반 게시판
-                classOneVIew.start("4");
+                String userdept = loginUserContext.get(0).getDepartment();
+                if(userdept.equals("1")){
+                    classOneView.start("4");
+                }
+                if(!(userdept.equals("1"))){
+                    logInfo("\n" +
+                            "┌───────────────┐\n" +
+                            "    1반 친구만!    \n" +
+                            "└───────────────┘\n" +
+                            "　　ᕱ ᕱ ||\n" +
+                            "　 ( ･ω･ ||\n" +
+                            "　 /　つΦ");
+                }
             }
             if (selectNum.equals("5")) {
                 anonymousView.start(selectNum);
