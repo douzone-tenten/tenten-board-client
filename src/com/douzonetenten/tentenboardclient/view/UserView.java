e68b64615543c3b601e1e8c9b7ad22308d419e74
d0uwhs <d0uwhs@gmail.com>
2022-08-18 16:43:53 +0900

    Docs: 유저뷰 주석 작성

--- src/com/douzonetenten/tentenboardclient/view/UserView.java
+++ src/com/douzonetenten/tentenboardclient/view/UserView.java
@@ -9,13 +9,25 @@ import static com.douzonetenten.tentenboardclient.service.UserService.loginUserC
 import static com.douzonetenten.tentenboardclient.utils.ConsoleUtils.*;
 import static com.douzonetenten.tentenboardclient.utils.UserInterfaceUtils.uiTitle;
 
+/**
+ * 회원가입 공통 컴포넌트입니다.
+ * @author 김민준
+ */
 public class UserView {
     private UserController userController = new UserController();
     private Scanner scanner = new Scanner(System.in);
     private UserDto userDto = new UserDto();
     private LoginMainView loginMainView = new LoginMainView();
 
+
+    /**
+     * 회원가입 공통 컴포넌트입니다.
+     * @author 김민준
+     */
     public void insertUser() {
+        /**
+         * 입력받은 유저의 정보로 회원가입을 합니다.
+         */
         clearConsole();
         uiTitle("회원가입");
 
@@ -28,28 +40,33 @@ public class UserView {
             System.out.println("비밀번호를 다시 입력하세요.");
             String passwordCheck = scanner.nextLine();
 
+            if (!password.equals(passwordCheck)) {
+                System.out.println("비밀번호가 일치하지 않습니다.");
+            }
+
             if (password.equals(passwordCheck)) {
                 userDto.setPassword(password);
                 break;
-            }
-            if (!password.equals(passwordCheck)) {
-                System.out.println("비밀번호가 일치하지 않습니다.");
             }
         }
 
         System.out.println("이름을 입력하세요.");
         userDto.setName(scanner.nextLine());
+
         while (true) {
             System.out.println("반을 입력하세요. (1반이면 1, 2반이면 2)");
             String department = scanner.nextLine();
+
             if (!(department.equals("1") || department.equals("2"))) {
                 System.out.println("1 또는 2만 입력해주세요.");
             }
+
             if ((department.equals("1") || department.equals("2"))) {
                 userDto.setDepartment(department);
                 break;
             }
         }
+
         int result = userController.insertUser(userDto);
         if (result == 1){
             System.out.println("회원가입에 성공했습니다.");
