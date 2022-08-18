045f8498e193a5635753f9772d21940b1ff71005
ms_hwang <dnfvmvhtm@naver.com>
2022-08-19 01:13:48 +0900

    더존 2반 게시판 최종 점검 완료

--- src/com/douzonetenten/tentenboardclient/view/boards/ClassTwoView.java
+++ src/com/douzonetenten/tentenboardclient/view/boards/ClassTwoView.java
@@ -38,7 +38,7 @@ public class ClassTwoView {
              * @author 황명수
              */
             if (!(loginUserContext.get(0).getDepartment().equals("2"))) {
-                logError("접근 권한이 없습니다");
+                logWarn("접근 권한이 없습니다");
                 break;
             }
             clearConsole();
@@ -69,7 +69,7 @@ public class ClassTwoView {
 
             logInfo("원하는 메뉴를 선택하세요");
             logInfo("b. 뒤로 가기   r.상세조회   w. 글쓰기");
-            char answer = sc.next().charAt(0);
+            String answer = sc.nextLine();
 
             /**
              * answer.equals("w") || answer.equals("W") = 입력받은 Scanner가 w,W 이면 글쓰기
@@ -77,7 +77,7 @@ public class ClassTwoView {
              * @author 황명수
              */
 
-            if (answer == 'w' || answer == 'W') {
+            if (answer.equals("w") || answer.equals("W")) {
                 dztwoinsert();
             }
 
@@ -86,7 +86,7 @@ public class ClassTwoView {
              * @author 황명수
              */
 
-            if (answer == 'b' || answer == 'B') {
+            if (answer.equals("b") || answer.equals("B")) {
                 break;
             }
 
@@ -96,7 +96,7 @@ public class ClassTwoView {
              * @author 황명수
              */
 
-            if (answer == 'r' || answer == 'R') {
+            if (answer.equals("r") || answer.equals("R")) {
 //                clearConsole();
                 logInfo("상세조회할 게시글 번호를 입력하세요 : ");
                 int detail = sc.nextInt();
@@ -122,7 +122,10 @@ public class ClassTwoView {
         }
 
         while (true) {
-
+//            ArrayList<JoinPostDto> boardDtoArrayList = douZoneTwoController.douzoneTwoDetailSelect(postId);
+//            for (JoinPostDto joinPostDto : boardDtoArrayList) {
+//                System.out.println(joinPostDto.detailPostToString());
+//            }
 
 
             String answer = sc.nextLine();
@@ -166,7 +169,7 @@ public class ClassTwoView {
          * @author 황명수
          */
 //        String a = sc.nextLine();
-        System.out.println("====================================================");
+        System.out.println("======================================");
 
         JoinPostDto joinPostDto = new JoinPostDto();
         logInfo("제목을 수정해주세요 : ");
@@ -176,27 +179,28 @@ public class ClassTwoView {
 
          int result =  douZoneTwoController.douzoneTwoUpdate(postId, test,body);
          if (result > 0){
+             joinPostDto.setPostTitle(test);
+             joinPostDto.setPostBody(body);
+             System.out.println(joinPostDto.getPostTitle());
+             System.out.println(joinPostDto.getPostBody());
+             logInfo("위 내용이 맞나요?");
+             logInfo("Y : 등록하기");
+             logInfo("B : 취소하기");
+
+             String select = sc.next();
+             if (select.equals("Y") || select.equals("y")) {
+
+                 logInfo("작성한 글이 등록되었습니다.");
+             }
+             if (select.equals("B")) {
+                 logInfo("글 작성을 취소합니다.");
+             }
              logInfo("게시글 수정을 성공했습니다.");
          }else {
              logWarn("본인이 작성한 게시글만 수정할 수 있습니다.");
          }
-        clearConsole();
-        joinPostDto.setPostTitle(test);
-        joinPostDto.setPostBody(body);
-        System.out.println(joinPostDto.getPostTitle());
-        System.out.println(joinPostDto.getPostBody());
-        logInfo("위 내용이 맞나요?");
-        logInfo("Y : 등록하기");
-        logInfo("B : 취소하기");
 
-        String select = sc.next();
-        if (select.equals("Y") || select.equals("y")) {
 
-            logInfo("작성한 글이 등록되었습니다.");
-        }
-        if (select.equals("B")) {
-            logInfo("글 작성을 취소합니다.");
-        }
 
     }
     public void dztwoinsert() {
@@ -204,7 +208,7 @@ public class ClassTwoView {
          * dztwoinsert() = 사용자가 더존 2반 게시판에 글을 작성할 수 있는 메소드
          * @author 황명수
          */
-
+        sc.nextLine();
         PostDto postDto = new PostDto();
         logInfo("제목을 입력하세요 : ");
         String douzone_postTitle = sc.nextLine();
@@ -212,14 +216,13 @@ public class ClassTwoView {
         String douzone_postBody = sc.nextLine();
         postDto.setPostTitle(douzone_postTitle);
         postDto.setPostBody(douzone_postBody);
-        clearConsole();
         System.out.println(postDto.getPostTitle());
         System.out.println(postDto.getPostBody());
         logInfo("위 내용이 맞나요?");
         logInfo("Y : 등록하기");
         logInfo("B : 취소하기");
 
-        String select = sc.next();
+        String select = sc.nextLine();
         if (select.equals("Y") || select.equals("y")) {
             logInfo("작성한 글이 등록되었습니다.");
             douZoneTwoController.dozoneTwoInsert(postDto, "5");
@@ -228,7 +231,7 @@ public class ClassTwoView {
             logInfo("글 작성을 취소합니다.");
         }
     }
-    public void dzTwoDelete(int port_id){
+    public void dzTwoDelete(int postId){
         /**
          * dzTwoDelete() = 사용자가 상세보기한 더존 2반 게시판 게시글을 삭제할 수 있는 메소드(단, 본인이 작성한 것만 가능하다)
          * logInfo() = 사용자에게 정보를 전달하는 메소드
@@ -239,7 +242,7 @@ public class ClassTwoView {
        logInfo("선택한 게시글을 삭제하겠습니까? (y/n)");
         String a = sc.nextLine();
         if ( a.equals("y") || a.equals("Y") ){
-            int result = douZoneTwoController.douzoneTwoDelete(port_id);
+            int result = douZoneTwoController.douzoneTwoDelete(postId);
             if (result > 0){
                 logInfo("선택한 게시글 삭제가 완료 되었습니다.");
 
