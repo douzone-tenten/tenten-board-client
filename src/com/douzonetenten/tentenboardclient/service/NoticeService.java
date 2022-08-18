253e052689c4531248a369816c570555bd381a29
d0uwhs <d0uwhs@gmail.com>
2022-08-18 15:20:54 +0900

    Temp

--- src/com/douzonetenten/tentenboardclient/service/NoticeService.java
+++ src/com/douzonetenten/tentenboardclient/service/NoticeService.java
@@ -4,8 +4,6 @@ package com.douzonetenten.tentenboardclient.service;
 import com.douzonetenten.tentenboardclient.dao.PostDao;
 import com.douzonetenten.tentenboardclient.dao.Notice_postDao;
 import com.douzonetenten.tentenboardclient.dto.Notice_JoinPostDto;
-import com.douzonetenten.tentenboardclient.dao.NoticePostDao;
-import com.douzonetenten.tentenboardclient.dto.JoinPostDto;
 import com.douzonetenten.tentenboardclient.dto.PostDto;
 
 import java.sql.Connection;
@@ -15,12 +13,10 @@ import static com.douzonetenten.tentenboardclient.common.DBConnector.*;
 
 public class NoticeService {
     private final PostDao postDao = new PostDao();
-    //private final Notice_JoinPostDto noticeJoinPostDto = new Notice_JoinPostDto();
-
-    public ArrayList<Notice_JoinPostDto> FindByAll(long post_id){
+    public ArrayList<Notice_JoinPostDto> FindByAll(long postId){
         Connection connection = getConnection();
-        ArrayList<Notice_JoinPostDto> notice_list = Notice_postDao.FindByAll(connection, post_id);
-        return notice_list;
+        ArrayList<Notice_JoinPostDto> noticeList = Notice_postDao.FindByAll(connection, postId);
+        return noticeList;
     }
 
     public int insertPost(PostDto postDto, String boardNumber) {
@@ -32,9 +28,9 @@ public class NoticeService {
         return result;
     }
 
-    public static int deletePost(String post_id){
+    public static int deletePost(String postId){
         Connection connection = getConnection();
-        int result = Notice_postDao.deletePost(connection, post_id);
+        int result = Notice_postDao.deletePost(connection, postId);
         if(result > 0){
             commit(connection);
         }else {
